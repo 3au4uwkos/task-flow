77ed0ceef67b49f1ea997143b87854954da7ee30
Матвей <super.matvei20120@yandex.ru>
2025-09-29 15:52:20 +0300

    Created basics of Laravel app plus initialized connection with DB

---
+++ backend/app/Models/Core/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Core;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'priority',
+    ];
+}
--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,6 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
+    // TODO реализовать чтение из .env
     'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
